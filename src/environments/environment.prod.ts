f6377d6ce4620d81198db1f42f15593dc3428df5
Stone C. Lasley <stonelasley@gmail.com>
2018-04-20 13:55:20 -0600

    fix(environment.prod): bucket directory name fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  bucket: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/vendor-label',
-  assets: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/vendor-label/assets',
+  bucket: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/vendor-labels',
+  assets: 'https://s3-us-west-1.amazonaws.com/apps.blendtec.com/vendor-labels/assets',
 };
